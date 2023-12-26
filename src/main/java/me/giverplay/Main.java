705a4e9fb38f804429054882a4b734e0def28b29
GiverPlay007 <play.giver.007@gmail.com>
2023-12-25 21:52:25 -0300

    Add JSON dependency

--- src/main/java/me/giverplay/Main.java
+++ src/main/java/me/giverplay/Main.java
@@ -1,7 +1,11 @@
 package me.giverplay;
 
+import org.json.JSONObject;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello");
+    JSONObject json = new JSONObject();
+    json.put("message", "Hello");
+    System.out.println(json);
   }
 }
